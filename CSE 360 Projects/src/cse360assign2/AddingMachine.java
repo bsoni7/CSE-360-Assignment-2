21e9242d304de9445162caef2912ce009cf494ac
bhavica soni <bhavica.soni@gmail.com>
2019-10-08 18:04:16 -0700

    First commit of Assignment 2. No changes to code made.

---
+++ CSE 360 Projects/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,51 @@
+/**
+ * Bhavica Soni
+ * ID: 1209105868
+ * CSE 360 Assignment 2
+ * This part of the assignment is simply to add comments.
+ * Most methods are blank. 
+ */
+
+package cse360assign2;
+
+public class AddingMachine {
+
+	private int total;
+	
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * Get the number of total machines
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * Add a machine
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * Subtract a machine
+	 */
+	public void subtract (int value) {
+		
+	}
+		
+	
+	public String toString () {
+		return "";
+	}
+
+	/**
+	 * Clear the list
+	 */
+	public void clear() {
+	
+	}
+}
